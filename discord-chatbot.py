eedba3d98825ec4ab3302e019a7b1fe21fae4c0c
jeff-tor <torjiafu@gmail.com>
2023-07-08 23:28:29 +0800

    Added function for discordBot

--- discord-chatbot.py
+++ discord-chatbot.py
@@ -11,32 +11,32 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
 class DiscordChatBot:
     def chat_bot_initialize(self):
-        discord_bot = discord.Client()
+        bot = discord.Client()
 
-@bot.event
-async def on_ready():
+        @bot.event
+        async def on_ready():
 
-    guild_count = 0
+            guild_count = 0
 
-    # LOOPS THROUGH ALL THE GUILD / SERVERS THAT THE BOT IS ASSOCIATED WITH.
-    for guild in bot.guilds:
+            # LOOPS THROUGH ALL THE GUILD / SERVERS THAT THE BOT IS ASSOCIATED WITH.
+            for guild in bot.guilds:
 
-        # PRINT THE SERVER'S ID AND NAME.
-        print(f"- {guild.id} (name: {guild.name})")
+                # PRINT THE SERVER'S ID AND NAME.
+                print(f"- {guild.id} (name: {guild.name})")
 
-        # INCREMENTS THE GUILD COUNTER.
-        guild_count = guild_count + 1
+                # INCREMENTS THE GUILD COUNTER.
+                guild_count = guild_count + 1
 
-    # PRINTS HOW MANY GUILDS / SERVERS THE BOT IS IN.
-    print("SampleDiscordBot is in " + str(guild_count) + " guilds.")
+            # PRINTS HOW MANY GUILDS / SERVERS THE BOT IS IN.
+            print("SampleDiscordBot is in " + str(guild_count) + " guilds.")
 
-# EVENT LISTENER FOR WHEN A NEW MESSAGE IS SENT TO A CHANNEL.
-@bot.event
-async def on_message(message):
-    # CHECKS IF THE MESSAGE THAT WAS SENT IS EQUAL TO "HELLO".
-    if message.content == "hello":
-        # SENDS BACK A MESSAGE TO THE CHANNEL.
-        await message.channel.send("hey dirtbag")
+        # EVENT LISTENER FOR WHEN A NEW MESSAGE IS SENT TO A CHANNEL.
+        @bot.event
+        async def on_message(message):
+            # CHECKS IF THE MESSAGE THAT WAS SENT IS EQUAL TO "HELLO".
+            if message.content == "hello":
+                # SENDS BACK A MESSAGE TO THE CHANNEL.
+                await message.channel.send("hey dirtbag")
 
 # EXECUTE BOT g
 bot.run(DISCORD_TOKEN)
