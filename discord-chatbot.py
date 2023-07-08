1e0b03fea94ae554df9dab75cf2da7f3eca22597
jeff-tor <torjiafu@gmail.com>
2023-07-08 21:17:51 +0800

    Created class DiscordChatBot

---
+++ discord-chatbot.py
@@ -0,0 +1,10 @@
+import discord
+import requests
+
+
+class DiscordChatBot:
+    def chat_bot_initialize(self):
+        discord.Client.application_info()
+
+if __name__ == "__main__":
+    DiscordChatBot.chat_bot_initialize()
